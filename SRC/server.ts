439bbb6bcbeb920186c3b94787623d601cbf7835
shlomosavyon <shlomosavyon@gmail.com>
2026-02-06 18:36:21 -0500

    Fix import statement for scheduler module

--- SRC/server.ts
+++ SRC/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import QRCode from 'qrcode';
 import { getWhatsAppService } from './whatsapp-service';
-import { getScheduler } from './scheduler.js';
+import { getScheduler } from './scheduler';
 import { notificationService } from './notification-service';
 
 const app = express();
